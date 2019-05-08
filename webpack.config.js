9b4f631bbf190d35ce3436266c0f2d048c011c5f
Saad Salman <saadkhanuni@gmail.com>
2019-05-08 11:35:05 +0500

    UglifyJsPlugin Edited

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,87 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var CompressionPlugin = require('compression-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var path = require('path');
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CompressionPlugin = require("compression-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+var path = require("path");
 
 module.exports = {
   entry: {
-    'polyfills': './client/polyfills.ts',
-    'vendor': './client/vendor.ts',
-    'main': './client/app.ts'
+    polyfills: "./client/polyfills.ts",
+    vendor: "./client/vendor.ts",
+    main: "./client/app.ts"
   },
 
   resolve: {
-    extensions: ['.ts', '.js', '.scss', '.css']
+    extensions: [".ts", ".js", ".scss", ".css"]
   },
 
   module: {
-    rules: [{
-      test: /\.ts$/,
-      use: [{
-        loader: 'awesome-typescript-loader',
-        options: { configFileName: path.resolve(__dirname, 'tsconfig.json') }
-      }, 'angular2-template-loader']
-    },
-    {
-      test: /\.html$/,
-      use: 'html-loader'
-    },
-    {
-      test: /\.scss$/,
-
-      loaders: ['to-string-loader',].concat(ExtractTextPlugin.extract({
-        fallback: "style-loader",
+    rules: [
+      {
+        test: /\.ts$/,
         use: [
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' }
+          {
+            loader: "awesome-typescript-loader",
+            options: {
+              configFileName: path.resolve(__dirname, "tsconfig.json")
+            }
+          },
+          "angular2-template-loader"
         ]
-      }))
-    }
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      {
+        test: /\.scss$/,
+
+        loaders: ["to-string-loader"].concat(
+          ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: [{ loader: "css-loader" }, { loader: "sass-loader" }]
+          })
+        )
+      }
     ]
   },
 
   plugins: [
+    new ExtractTextPlugin("stylesheet.css"),
 
-    new ExtractTextPlugin('stylesheet.css'),
-
-    new CopyWebpackPlugin([{ from: path.resolve(__dirname, 'client/assets') }]),
+    new CopyWebpackPlugin([{ from: path.resolve(__dirname, "client/assets") }]),
     // Workaround for angular/angular#11580
 
     new webpack.ContextReplacementPlugin(
       /angular(\\|\/)core(\\|\/)@angular/,
-      path.resolve(__dirname, './client')
+      path.resolve(__dirname, "./client")
     ),
 
     new HtmlWebpackPlugin({
-      template: 'client/index.html'
+      template: "client/index.html"
     })
   ],
 
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js'
-  },
+    path: path.join(__dirname, "public"),
+    filename: "[name].js",
+    chunkFilename: "[id].chunk.js"
+  }
 };
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   module.exports.devtool = false;
   module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
+    new UglifyJsPlugin({
+      uglifyOptions: {
         warnings: false,
-        pure_funcs: ['console.log', 'window.console.log.apply']
-      },
-      mangle: false
+        ie8: false,
+        output: {
+          comments: false
+        }
+      }
     }),
     new webpack.optimize.AggressiveMergingPlugin(),
     new CompressionPlugin({
@@ -84,5 +91,5 @@ if (process.env.NODE_ENV === 'production') {
       threshold: 10240,
       minRatio: 0
     })
-  ])
+  ]);
 }
